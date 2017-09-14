37e21b63dc202cc549035aab59dd412d0c3bce2d
komisienko <komisienko@gmail.com>
2017-09-14 14:52:53 +0300

    added tc

--- admin_panel/js/all.js
+++ admin_panel/js/all.js
@@ -1,6 +1,4 @@
-$(document).ready(function () {
-	$('#employees').dataTable();
-});
+
 //текущаяя дата
 document.getElementById("current_data").innerHTML = get_date();
 
@@ -13,20 +11,3 @@ function get_date() {
 	return hours + ':' + minutes + ' ' + days[d.getDay()] + '</br>' + d.getDate() + '/' + months[d.getMonth()] + '/' + d.getFullYear();
 }
 
-// Работа с таблицей
-$(document).ready(function () {
-	$('#employees').dataTable();
-});
-	(function () { //чтобы не засорять глобал.
-		var inputs = document.getElementById('all').getElementsByTagName('input');
-		document.getElementById('checkAll').addEventListener('click', function checkme(e, check) {
-			check = check === undefined ? false : true;
-			for (var i = 0, inputs_len = inputs.length; i < inputs_len; i++) {
-				if (!check && !inputs[i].checked) {
-					checkme(e, true);
-					return;
-				}
-				inputs[i].checked = check;
-			}
-		});
-	})();
