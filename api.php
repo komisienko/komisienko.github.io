183592bace0b2a4088e88f1b6fd8cb228e6311d5
komisienko <komisienko@gmail.com>
2018-02-07 14:20:09 +0200

    1

--- api.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $botToken="538675880:AAEpH-eMwHyQ804TqvhHy2y52Xa7SP91FuA";
-    $website = "php//input".$botToken;
-    
-    $update = file_get_contents($website."/getupdates");
-    print_r($update);
-    
-?>
