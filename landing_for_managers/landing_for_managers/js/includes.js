e8efadd22508c8fb908b6647a3449b5d4467bd42
Serhiy <komisienko@gmail.com>
2018-06-25 19:21:11 +0300

    hotfix

--- landing_for_managers/landing_for_managers/js/includes.js
+++ landing_for_managers/landing_for_managers/js/includes.js
@@ -1,18 +1,30 @@
 "use strict";
 $(document).ready(function () {
-    $('.sidenav').sidenav();
+    $('.sidenav').sidenav({
+       
+    });
+          
+$('#nav-mobile').on("click", "a", function(){
+     let instance = M.Sidenav.getInstance('.sidenav');
+     $('.sidenav').sidenav('close');
+     event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top-54}, 700);
+})
 });
 
-$('#nav-mobile').on('click', function (event) {
+$('#nav-desctop').on('click',"a", function (event) {
     $(this).parent().find('a').parent().removeClass('active');
     $(this).parent().addClass('active');
+    
 });
 $(window).on('scroll', function () {
     $('.target').each(function () {
-        if ($(window).scrollTop() >= $(this).position().top-70) {
+        if ($(window).scrollTop() >= $(this).position().top-120) {
             var id = $(this).attr('id');
-            $('#nav-mobile a').parent().removeClass('active');
-            $('#nav-mobile a[href="#' + id + '"]').parent().addClass('active');
+            $('#nav-desctop a').parent().removeClass('active');
+            $('#nav-desctop a[href="#' + id + '"]').parent().addClass('active');
         }
     });
 });
