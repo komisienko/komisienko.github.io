8dbe62b1f720f54c670e16ea1a54a485ac99a530
Serhiy <komisienko@gmail.com>
2018-06-25 18:21:48 +0300

    test

--- landing_for_managers/landing_for_managers/js/includes.js
+++ landing_for_managers/landing_for_managers/js/includes.js
@@ -1,3 +1,18 @@
-  $(document).ready(function(){
+"use strict";
+$(document).ready(function () {
     $('.sidenav').sidenav();
-  });
+});
+
+$('#nav-mobile').on('click', function (event) {
+    $(this).parent().find('a').parent().removeClass('active');
+    $(this).parent().addClass('active');
+});
+$(window).on('scroll', function () {
+    $('.target').each(function () {
+        if ($(window).scrollTop() >= $(this).position().top) {
+            var id = $(this).attr('id');
+            $('#nav-mobile a').parent().removeClass('active');
+            $('#nav-mobile a[href="#' + id + '"]').parent().addClass('active');
+        }
+    });
+});
