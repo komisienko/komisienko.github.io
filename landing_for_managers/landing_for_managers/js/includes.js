c5c1bdd46a3fd00101fcc0d77aa98b600a76f018
Serhiy <komisienko@gmail.com>
2018-06-25 19:40:13 +0300

    hotfix

--- landing_for_managers/landing_for_managers/js/includes.js
+++ landing_for_managers/landing_for_managers/js/includes.js
@@ -1,8 +1,7 @@
 "use strict";
 $(document).ready(function () {
-    $('.sidenav').sidenav({
-       
-    });
+    $('.sidenav').sidenav();
+    $('.modal').modal();
           
 $('#nav-mobile').on("click", "a", function(){
      let instance = M.Sidenav.getInstance('.sidenav');
@@ -17,11 +16,14 @@ $('#nav-mobile').on("click", "a", function(){
 $('#nav-desctop').on('click',"a", function (event) {
     $(this).parent().find('a').parent().removeClass('active');
     $(this).parent().addClass('active');
-    
+     event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top-64}, 700);
 });
 $(window).on('scroll', function () {
     $('.target').each(function () {
-        if ($(window).scrollTop() >= $(this).position().top-120) {
+        if ($(window).scrollTop() >= $(this).position().top-150) {
             var id = $(this).attr('id');
             $('#nav-desctop a').parent().removeClass('active');
             $('#nav-desctop a[href="#' + id + '"]').parent().addClass('active');
