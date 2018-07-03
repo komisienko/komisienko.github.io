24969f9be26f89399dd619ba3571886dd67ca5a5
Serhiy <komisienko@gmail.com>
2018-07-03 13:02:59 +0300

    htofix

--- midas_2/js/includes.js
+++ midas_2/js/includes.js
@@ -151,7 +151,7 @@ $(".main").onepage_scroll({
    easing: "cubic-bezier(0.175, 0.885, 0.420, 1.210)", // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in", "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
    animationTime: 1000, // AnimationTime let you define how long each section takes to animate
    pagination: true, // You can either show or hide the pagination. Toggle true for show, false for hide.
-   updateURL: true, // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+   updateURL: false, // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
    beforeMove: function(index) {
         if ($(window).width() > 992) {
 //             if($('body').hasClass('viewing-page-1')){
