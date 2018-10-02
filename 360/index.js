aec0fc3edeced0ca462c6aac268f98104b82b17c
Crinson <komisienko@gmail.com>
2018-10-02 21:37:19 +0300

    3

--- 360/index.js
+++ 360/index.js
@@ -1,18 +1,4 @@
-/*
- * Copyright 2016 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 'use strict';
 
 (function() {
@@ -69,7 +55,11 @@
 
   // Initialize viewer.
   var viewer = new Marzipano.Viewer(panoElement, viewerOpts);
-
+//NEW
+    var deviceOrientationControlMethod = new DeviceOrientationControlMethod();
+var controls = viewer.controls();
+controls.registerMethod('deviceOrientation', deviceOrientationControlMethod);
+    //eND
   // Create scenes.
   var scenes = data.scenes.map(function(data) {
     var urlPrefix = "tiles";
@@ -87,6 +77,40 @@
       view: view,
       pinFirstLevel: true
     });
+      scene.switchTo();
+
+// Set up control for enabling/disabling device orientation.
+
+var enabled = false;
+
+var toggleElement = document.getElementById('toggleDeviceOrientation');
+
+function enable() {
+  deviceOrientationControlMethod.getPitch(function(err, pitch) {
+    if (!err) {
+      view.setPitch(pitch);
+    }
+  });
+  controls.enableMethod('deviceOrientation');
+  enabled = true;
+  toggleElement.className = 'enabled';
+}
+
+function disable() {
+  controls.disableMethod('deviceOrientation');
+  enabled = false;
+  toggleElement.className = '';
+}
+
+function toggle() {
+  if (enabled) {
+    disable();
+  } else {
+    enable();
+  }
+}
+
+toggleElement.addEventListener('click', toggle);
 
     // Create link hotspots.
     data.linkHotspots.forEach(function(hotspot) {
