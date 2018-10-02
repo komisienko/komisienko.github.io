851f375a7cf5f972f48334e8e3349fcf55f56a0b
Crinson <komisienko@gmail.com>
2018-10-02 21:11:22 +0300

    360

---
+++ 360/data.js
@@ -0,0 +1,41 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "name": "PANO_20181002_205546_0",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
