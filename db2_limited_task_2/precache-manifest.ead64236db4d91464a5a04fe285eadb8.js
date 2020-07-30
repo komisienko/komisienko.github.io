8eff677c0ffd8dbbeb39a09a7230140b5bc74056
Sergei <komisienko@gmail.com>
2020-07-30 10:09:00 +0300

    Done

---
+++ db2_limited_task_2/precache-manifest.ead64236db4d91464a5a04fe285eadb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d2ea64b556ec0a49757c0b2612da6b",
+    "url": "/komisienko.github.io/DB2_LIMITED_TASK_2/index.html"
+  },
+  {
+    "revision": "0464866c018f8d33cb3d",
+    "url": "/komisienko.github.io/DB2_LIMITED_TASK_2/static/js/2.9bfb1fd2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/komisienko.github.io/DB2_LIMITED_TASK_2/static/js/2.9bfb1fd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b71dfa1592f06dd85dde",
+    "url": "/komisienko.github.io/DB2_LIMITED_TASK_2/static/js/main.7626489f.chunk.js"
+  },
+  {
+    "revision": "6e1d53e5c03699dbc8d1",
+    "url": "/komisienko.github.io/DB2_LIMITED_TASK_2/static/js/runtime-main.dbd7d129.js"
+  }
+]);
